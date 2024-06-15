2031a61d658e348d638a727ab6f1a8865002f0d1
dhruvghoghari <128144781+dhruvghoghari@users.noreply.github.com>
2024-06-15 07:11:47 +0530

    14/06/2024

--- lib/views/Home_View.dart
+++ lib/views/Home_View.dart
@@ -11,12 +11,17 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHight = MediaQuery.of(context).size.height;
     return Scaffold(
       drawer: CustomDrawer(),
       appBar: AppBar(title: Text("Home"),),
       body: Column(
         children: [
-
+          Container(
+            height: screenWidth * 0.5,
+            width: screenHight * 0.5,
+          )
         ],
       ),
     );
